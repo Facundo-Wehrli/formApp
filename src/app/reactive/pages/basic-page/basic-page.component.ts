da43cd64c4f03dd9b4bb0f26c32ce5d1efd6e232
Facundo <wehrlifacundo9@gmail.com>
2023-08-08 11:09:16 -0300

    feat: reset form values & validators

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,27 +1,47 @@
-import { Component } from '@angular/core';
-import { FormBuilder,  FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 2500,
+  inStorage: 2,
+};
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [],
 })
-export class BasicPageComponent {
-  //sin formbuilder
+export class BasicPageComponent implements OnInit {
+  // formulario sin FormBuilder
   // public myForm: FormGroup = new FormGroup({
   //   name: new FormControl(''),
   //   price: new FormControl(0),
   //   inStorage: new FormControl(0),
   // })
 
-  //con formbuilder
+  //con FormBuilder
   public myForm: FormGroup = this.fb.group({
-    name: [''],
-    price: [0],
-    inStorage: [0],
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   });
   constructor(private fb: FormBuilder) {}
 
-  onSave():void{
-    console.log(this.myForm.value)
+  ngOnInit(): void {
+    this.myForm.reset(rtx5090);
+  }
+
+  onSave(): void {
+    /* The line `if (this.myForm.invalid) return;` is checking if the form is invalid. If the form is
+    invalid, it will immediately return from the function without executing any further code. This
+    is typically used to prevent the form from being submitted or any further actions from being
+    taken if the form is not valid. */
+    if (this.myForm.invalid) return;
+    console.log(this.myForm.value);
+    /* `this.myForm.reset({price:10,inStorage:0});` is resetting the form to its initial state. It sets
+    the values of the form controls `price` and `inStorage` to 10 and 0 respectively. This is useful
+    for scenarios where you want to reset the form after it has been submitted or when you want to
+    clear the form values. */
+    this.myForm.reset({ price: 0, inStorage: 0 });
   }
 }
