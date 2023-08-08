123ddcb3a99b04645b4fcb50c37a0d26ba4070d4
Facundo <wehrlifacundo9@gmail.com>
2023-08-08 09:57:25 -0300

    chore(reactive module): import ReactiveFormsModule

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
+
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
-import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
 import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,6 @@ import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.componen
     SwitchesPageComponent,
     DynamicPageComponent,
   ],
-  imports: [CommonModule, ReactiveRoutingModule],
+  imports: [CommonModule, ReactiveRoutingModule, ReactiveFormsModule],
 })
 export class ReactiveModule {}
