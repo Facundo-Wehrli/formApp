ccc9ff818b14c170ff043ab687f4b99a3060ed79
Facundo <wehrlifacundo9@gmail.com>
2023-08-14 17:40:27 -0300

    feat: email validators & common  pattern usages

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { cantBePepito } from 'src/app/shared/validators/validators';
+import {
+  cantBePepito,
+  emailPattern,
+  firstNameAndLastnamePattern,
+} from 'src/app/shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
 })
 export class RegisterPageComponent {
   public myForm: FormGroup = this.fb.group({
-    name: ['', Validators.required],
-    email: ['', Validators.required],
+    name: [
+      '',
+      [Validators.required, Validators.pattern(firstNameAndLastnamePattern)],
+    ],
+    email: ['', [Validators.required, Validators.pattern(emailPattern)]],
     username: ['', [Validators.required, cantBePepito]],
-    password: ['', Validators.required, Validators.minLength(6)],
-    password2: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
   });
   constructor(private fb: FormBuilder) {}
   isValidField(field: string) {
