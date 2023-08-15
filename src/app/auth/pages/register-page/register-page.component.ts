b4bd4ed00654a52febd398cf58c886b67d1a7f3a
Facundo <wehrlifacundo9@gmail.com>
2023-08-15 00:09:21 -0300

    refactor: turn validators to a service

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from 'src/app/shared/service/validators.service';
 import {
   cantBePepito,
   emailPattern,
@@ -13,16 +14,30 @@ export class RegisterPageComponent {
   public myForm: FormGroup = this.fb.group({
     name: [
       '',
-      [Validators.required, Validators.pattern(firstNameAndLastnamePattern)],
+      [
+        Validators.required,
+        Validators.pattern(this.validatorsService.firstNameAndLastnamePattern),
+      ],
     ],
-    email: ['', [Validators.required, Validators.pattern(emailPattern)]],
-    username: ['', [Validators.required, cantBePepito]],
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(this.validatorsService.emailPattern),
+      ],
+    ],
+    username: ['', [Validators.required, this.validatorsService.cantBePepito]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]],
   });
-  constructor(private fb: FormBuilder) {}
+
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService
+  ) {}
   isValidField(field: string) {
     //TODO obtener validación de servicio
+    return this.validatorsService.isValidField(this.myForm, field);
   }
 
   onSubmit() {
