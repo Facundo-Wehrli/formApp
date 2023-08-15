a8260840dd55683ca483d3b6cf9b4ea8228accf5
Facundo <wehrlifacundo9@gmail.com>
2023-08-15 09:25:34 -0300

    feat: compare first password to second password validation

--- src/app/shared/service/validators.service.ts
+++ src/app/shared/service/validators.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
-import { FormControl, FormGroup, ValidationErrors } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+} from '@angular/forms';
 
 @Injectable({ providedIn: 'root' })
 export class ValidatorsService {
@@ -21,5 +26,19 @@ export class ValidatorsService {
   public isValidField(form: FormGroup, field: string): boolean | null {
     /*  check if a specific form field has errors and has been touched by the user. */
     return form.controls[field].errors && form.controls[field].touched;
+  }
+  isFieldOneEqualFieldTwo(field1: string, field2: string) {
+    return (formGroup: FormGroup): ValidationErrors | null => {
+      const fieldValue1 = formGroup.get(field1)?.value;
+      const fieldValue2 = formGroup.get(field2)?.value;
+
+      if (fieldValue1 !== fieldValue2) {
+        formGroup.get(field2)?.setErrors({ notEqual: true });
+        return { notEqual: true };
+      }
+      formGroup.get(field2)?.setErrors(null);
+
+      return null;
+    };
   }
 }
